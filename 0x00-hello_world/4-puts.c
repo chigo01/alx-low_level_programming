8dbced4aa4772b3576ccd8de5c9a210317e04acd
Favour Baruch <78267322+chigo01@users.noreply.github.com>
2022-06-30 08:24:21 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0
- *     */
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	return (0);
 }
-
