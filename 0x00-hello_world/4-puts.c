f79be85642bbda044dae2cd69e118879a20de32e
chigo01 <baruchfavour01@gmail.com>
2022-06-30 00:14:59 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+int main()
+
+{
+
+	    /*
+	     *
+	     *      * main -block
+	     *
+	     *           * return 0;
+	     *
+	     *                */
+
+
+
+	    puts("Programming is like building a multilingual puzzle\n");
+
+
+
+	        return 0;
+
+}
