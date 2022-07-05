de942439fcb29c5a26b29a2c5dbeb0ebe771c9cb
chigo01 <baruchfavour01@gmail.com>
2022-07-05 08:18:26 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-my readme 
+my file 
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include "_putchar.c"
-#include "main.c"
-
-
-
-
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -20,5 +12,3 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-
-#endif
