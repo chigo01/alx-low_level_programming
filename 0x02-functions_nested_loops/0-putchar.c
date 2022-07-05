eedd6d2e063dc8ccf06c53da0145b34d2b092f2c
Favour Baruch <78267322+chigo01@users.noreply.github.com>
2022-07-05 15:04:56 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
-#include "main.h"
 
+
+#include "main.h"
 /**
- * * main - prints putchar
- * * Return: always 0
- * */
+ * main - a program that prints __putchar
+ *
+ * Return: returns 0 always
+ */
 int main(void)
 {
-		_putchar('_');
-			_putchar('p');
-				_putchar('u');
-					_putchar('t');
-						_putchar('c');
-							_putchar('h');
-								_putchar('a');
-									_putchar('r');
-										_putchar('\n');
-
-											return (0);
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	_putchar('\n');
+	return (0);
 }
-
