293bd393d9ce4d2ea9653222d6f46e1811f018d1
Favour Baruch <78267322+chigo01@users.noreply.github.com>
2022-07-13 01:02:53 +0100

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,24 +1,22 @@
 #include "main.h"
 
 /**
- * puts_half - half of a string.
- * @str: string to be printed.
+ * puts2 - prints every other character of a string
+ * @str: the string to print
+ *
  */
-void puts_half(char *str)
+void puts2(char *str)
 {
-	int i = 0, len = 0, j;
+	int i;
+	int j;
 
-	while (str[i++])
-		len++;
-
-	if ((len % 2) == 0)
-		j = len / 2;
-
-	else
-		j = (len + 1) / 2;
-
-	for (i = j; i < len; i++)
-		_putchar(str[i]);
-
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		;
+	}
+	for (j = 0 ; j < i ; j = j + 2)
+	{
+		_putchar(str[j]);
+	}
 	_putchar('\n');
 }
