bef151f055ce34a907b6a44ac8421e06bd89ea92
chigo01 <baruchfavour01@gmail.com>
2022-07-22 13:06:47 +0100

    funcs

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -36,11 +36,11 @@ void simple_print_buffer(char *buffer, unsigned int size)
  */
 int main(void)
 {
-    char buffer[98] = {0x00};
+        char buffer[98] = {0x00};
 
-    simple_print_buffer(buffer, 98);
-    _memset(buffer, 0x01, 95);
-    printf("-------------------------------------------------\n");
-    simple_print_buffer(buffer, 98);    
-    return (0);
+        simple_print_buffer(buffer, 98);
+        _memset(buffer, 0x01, 95);
+        printf("-------------------------------------------------\n");
+        simple_print_buffer(buffer, 98);
+        return (0);
 }
