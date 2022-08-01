db60021f7c4ad32f668bc5a092c0c9149587bfdf
chigo01 <baruchfavour01@gmail.com>
2022-08-01 15:22:51 +0100

    1-init.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+    if (d)
+    {
+        (*d).name = name;
+        (*d).age = age;
+        (*d).owner = owner;
+    }
+}
